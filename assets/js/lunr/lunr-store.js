6ee75d1f2d53871ce091dc540b3c9579e5eb0d83
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-06-21 22:17:33 +0530

    update static deployment 21st june (night)

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "/first-post/",
         "teaser": "/assets/images/teaser-scenery.jpg"
       },{
-        "title": "GSoC: About the project",
+        "title": "GSoC: About the project (Part 1)",
         "excerpt":"I have been selected as a student developer in Google Summer of Code under the GeomScale organization &amp; will be spending the upcoming 10 Weeks working on the project of “Counting Linear Extensions”. This post will be for providing details regarding my project, mainly problem formulation and proposed solution. +...","categories": [],
         "tags": ["gsoc"],
         "url": "/gsoc-about-project/",
