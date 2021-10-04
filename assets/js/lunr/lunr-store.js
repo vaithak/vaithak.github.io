9265ded265fa4de8380a4378350b3d26d70e34ef
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2021-10-04 13:19:00 +0530

    new release

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,11 +1,5 @@
 var store = [{
-        "title": "First post",
-        "excerpt":"Hello World :wave: This is my first post on this blog and I really hope that it won’t be the last :sweat_smile:. Till now, I haven’t been very fond of writing big articles or essays, reasons of which are as follows: I am too lazy to write. As we know...","categories": [],
-        "tags": ["first-post"],
-        "url": "/first-post/",
-        "teaser": "/assets/images/teaser-scenery.jpg"
-      },{
-        "title": "GSoC: Project Overview (Part 1)",
+        "title": "Counting Linear Extensions: GSoC Project Overview",
         "excerpt":"I have been selected as a student developer in Google Summer of Code under the GeomScale organization &amp; will be spending the upcoming 10 Weeks working on the project of “Counting Linear Extensions”. This post will be for providing details regarding my project, mainly problem formulation and proposed solution. +...","categories": [],
         "tags": ["gsoc"],
         "url": "/gsoc-project-overview-part1/",
