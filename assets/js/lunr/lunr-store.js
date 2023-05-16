9e4c253973b1ec7752940aebf5ba1ec62e39a74c
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2023-05-16 13:20:39 +0530

    complete v1 of autodiff post

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["gsoc"],
         "url": "/gsoc-project-overview-part1/",
         "teaser": "/assets/images/gsoc.png"
+      },{
+        "title": "Automatic Differentiation",
+        "excerpt":"Given a continuos function $f(x)$, how do you compute it’s derivative $f’(x_0)$ for a particular input $x_0$ computationally? Similarly, for a vector valued function $f(\\vec{\\boldsymbol{x}})$, how do we compute its gradient $\\nabla_{\\vec{\\boldsymbol{x_0}}} f(\\vec{\\boldsymbol{x}})$? Where $\\vec{\\boldsymbol{x}}$ is an $n$ dimensional vector and $$ \\nabla f(\\vec{\\boldsymbol{x}}) = [ \\frac{\\partial f}{\\partial x_0}, \\frac{\\partial...","categories": [],
+        "tags": ["autodiff"],
+        "url": "/autodiff-clad/",
+        "teaser": "/assets/images/llvm.png"
       }]
